3acb2bb5a9c7faf51ff6ce67bda031460fa68a78
jonahtjandra <jonahtjandra@gmail.com>
2023-01-19 00:38:14 +0700

    fixed change detection

--- frontend/src/app/pages/dashboard/staff-dashboard/staff-dashboard.component.ts
+++ frontend/src/app/pages/dashboard/staff-dashboard/staff-dashboard.component.ts
@@ -10,25 +10,23 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class StaffDashboardComponent implements OnInit {
 
-  constructor(private StaffService: StaffService, private ref: ChangeDetectorRef, private router: Router, private route: ActivatedRoute) { }
+  constructor(private StaffService: StaffService, private ref: ChangeDetectorRef, private router: Router, private route: ActivatedRoute) {
+  }
   staff = []
   email = new FormControl('')
   status = new FormControl('')
-
-  refreshComp() {
-    this.router.routeReuseStrategy.shouldReuseRoute = () => false
-    this.router.onSameUrlNavigation = 'reload'
-    this.router.navigate(['./'], {
-      relativeTo: this.route
-    })
-  }
 
   addStaff() {
     let submit = confirm("Are you sure you want to add this staff?")
     if (submit) {
       const staffObj = { 'email': this.email.value, 'status': this.status.value }
       this.StaffService.addStaff(JSON.stringify(staffObj))
-      this.refreshComp()
+      this.email.setValue('')
+      this.status.setValue('')
+      this.StaffService.getStaff().subscribe((staff) => {
+        this.staff = staff["staff"]
+      })
+
     }
   }
   delStaff(email: string) {
@@ -36,7 +34,9 @@ export class StaffDashboardComponent implements OnInit {
     if (submit) {
       const payload = { 'email': email }
       this.StaffService.delStaff(JSON.stringify(payload))
-      this.refreshComp()
+      this.StaffService.getStaff().subscribe((staff) => {
+        this.staff = staff["staff"]
+      })
     }
   }
 

88e93bcaacbcd090b9e6fe760afbe600b968be44
Neil Kaushikkar <neil.kaushikkar@gmail.com>
2023-01-19 18:41:37 -0600

    Update SP23 schedule

--- frontend/src/app/pages/schedule/schedule.component.ts
+++ frontend/src/app/pages/schedule/schedule.component.ts
@@ -11,104 +11,98 @@ export interface ScheduleElemForTable {
 
 const SCHEDULE_DATA_FOR_TABLE: (ScheduleElemForTable )[] = [ // | WeekDivider
   {position: 1, name:'Week 1', type:'', date: '', isDivider: true, isDescription:false},
-  {position: 1, name: 'Info Video', type:'Lecture', date: '1/18/2022', isDivider: false, isDescription:false},
+  {position: 1, name: 'Info Video', type:'Lecture', date: '1/17/2023', isDivider: false, isDescription:false},
 
-  {position: 1, name:'Week 2', type:'', date: '', isDivider: true, isDescription:false},
-  {position: 1, name: 'Rust Setup', type:'Lecture', date: '1/25/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Rust Basics Part 1 - Variables, Control Flow', type:'Lecture', date: '1/27/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Optional Rust Practice', type:'Homework', date: '1/27/2022', isDivider: false, isDescription:false},
+  {position: 1, name: 'Week 2', type:'', date: '', isDivider: true, isDescription:false},
+  {position: 1, name: 'Rust Setup', type:'Lecture', date: '1/24/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Rust Basics Part 1 - Variables, Control Flow', type:'Lecture', date: '1/26/2023', isDivider: false, isDescription:false},
 
-  {position: 1, name:'Week 3', type:'', date: '', isDivider: true, isDescription:false},
-  {position: 1, name: 'Rust Basics Part 2 - Functions, Compound Data Types', type:'Lecture', date: '2/1/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Rust Basics Part 3 - Enums, Matching', type:'Lecture', date: '2/3/2022', isDivider: false, isDescription:false},
+  {position: 1, name: 'Week 3', type:'', date: '', isDivider: true, isDescription:false},
+  {position: 1, name: 'Rust Basics Part 2 - Functions, Compound Data Types', type:'Lecture', date: '1/31/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Release HW1: Variable Types and Control Flow', type: 'Homework', date: '1/31/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Rust Basics Part 2.5 - Basic Match Statements', type:'Lecture', date: '2/1/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'This will be a shorter lecture introducing the "match" keyword. This new lecture is here so that the Rust Basics 3 Lecture will not be extremely long.', type:'', date: '', isDivider: false, isDescription:true},
+  {position: 1, name: 'Release MP0: Calculator', type: 'MP', date: '2/1/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Rust Basics Part 3 - Enums, Matching', type:'Lecture', date: '2/2/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Release HW2: Enums, Option, and Result', type: 'Homework', date: '2/2/2023', isDivider: false, isDescription:false},
 
-  {position: 1, name: 'Release MP0', type:'MP', date: '2/3/2022', isDivider: false, isDescription:false},
+  {position: 1, name: 'Week 4', type:'', date: '', isDivider: true, isDescription:false},
+  {position: 1, name: 'Rust Basics Part 4 - Examples, Ownership', type:'Lecture', date: '2/7/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Release HW3: Scope and Ownership', type: 'Homework', date: '2/7/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Due: HW1', type: 'Due Date', date: '2/7/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Ownership in Rust - Ownership, Borrowing', type:'Lecture', date: '2/9/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Release HW4: Ownership and Vectors', type: 'Homework', date: '2/9/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Due: HW2', type: 'Due Date', date: '2/9/2023', isDivider: false, isDescription:false},
 
-  {position: 1, name: 'Release Homework 1 (Functions, Control Flow, Matching)', type:'Homework', date: '2/1/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Release Homework 2 (Some/None, Result, Enums)', type:'Homework', date: '2/3/2022', isDivider: false, isDescription:false},
+  {position: 1, name: 'Week 5', type:'', date: '', isDivider: true, isDescription:false},
+  {position: 1, name: 'Release MP1: K-Nearest Neighbors', type:'MP', date: '2/13/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Transferring Ownership', type: 'Lecture', date: '2/14/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Release HW5: More Ownership', type: 'Homework', date: '2/14/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Due: HW3', type: 'Due Date', date: '2/14/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Due: MP0', type: 'Due Date', date: '2/15/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Structs in Rust', type:'Lecture', date: '2/16/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Release HW6: Structs', type: 'Homework', date: '2/16/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Due: HW4', type: 'Due Date', date: '2/16/2023', isDivider: false, isDescription:false},
 
+  {position: 1, name: 'Week 6', type:'', date: '', isDivider: true, isDescription:false},
+  {position: 1, name: 'More Structs', type: 'Lecture', date: '2/21/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Release HW7: Impl with Structs', type: 'Homework', date: '2/21/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Due: HW5', type: 'Due Date', date: '2/21/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Release MP2: Hangman Game', type: 'MP', date: '2/22/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Structs and Generics', type: 'Lecture', date: '2/23/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Release HW8: Structs with Generics', type: 'Homework', date: '2/23/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Due: HW6', type: 'Due Date', date: '2/23/2023', isDivider: false, isDescription:false},
 
-  {position: 1, name:'Week 4', type:'', date: '', isDivider: true, isDescription:false},
-  {position: 1, name: 'Rust Basics Part 4 - Examples, Ownership', type:'Lecture', date: '2/8/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Ownership in Rust - Ownership, Borrowing', type:'Lecture', date: '2/10/2022', isDivider: false, isDescription:false},
+  {position: 1, name: 'Week 7', type:'', date: '', isDivider: true, isDescription:false},
+  {position: 1, name: 'Due: MP1', type: 'Due Date', date: '2/27/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Parallelism & Concurrency', type:'Lecture', date: '2/28/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Release HW9: Basic Parallelism', type: 'Homework', date: '2/28/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Due: HW7', type: 'Due Date', date: '2/28/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Threads & Ownership', type:'Lecture', date: '3/2/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Release HW10: More Threads', type: 'Homework', date: '3/2/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Due: HW8', type: 'Due Date', date: '3/2/2023', isDivider: false, isDescription:false},
 
-  {position: 1, name: 'Release MP1', type:'MP', date: '2/10/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Due: MP0', type:'MP', date: '2/11/2022', isDivider: false, isDescription:false},
+  {position: 1, name: 'Week 8', type:'', date: '', isDivider: true, isDescription:false},
+  {position: 1, name: 'Release MP3: MapReduce', type: 'MP', date: '3/6/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Threads & Messaging', type: 'Lecture', date: '3/7/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Release HW11 (Tentative): TBD', type: 'Homework', date: '3/7/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Due: HW9', type: 'Due Date', date: '3/7/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Due: MP2', type: 'Due Date', date: '3/8/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Shared State Messaging', type: 'Lecture', date: '3/9/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Release HW12 (Tentative): TBD', type: 'Homework', date: '3/9/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Due: HW10', type: 'Due Date', date: '3/9/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Final Project Group Formations Due', type: 'Final Project', date: '3/10/2023', isDivider: false, isDescription: false},
 
-  {position: 1, name: 'Due: Homework 1', type:'Homework', date: '2/8/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Release Homework 4 (Basic Ownership, basic transferring/borrowing)', type:'Homework', date: '2/10/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Due: Homework 2', type:'Homework', date: '2/8/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Release Homework 4 (Basic Ownership, basic transferring/borrowing)', type:'Homework', date: '2/10/2022', isDivider: false, isDescription:false},
+  {position: 1, name: 'Week 8.5', type:'', date: '', isDivider: true, isDescription:false},
+  {position: 1, name: 'Spring Break!', type:'', date: '', isDivider: false, isDescription:true},
 
-  {position: 1, name:'Week 5', type:'', date: '', isDivider: true, isDescription:false},
-  {position: 1, name: 'Transferring Ownership', type:'Lecture', date: '2/15/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Structs in Rust', type:'Lecture', date: '2/17/2022', isDivider: false, isDescription:false},
+  {position: 1, name: 'Week 9', type: '', date: '', isDivider: true, isDescription:false},
+  {position: 1, name: 'Rust Project Structure', type: 'Lecture', date: '3/21/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Special Topics Lecture (Traits, Async, Solana, Blockchain, etc...)', type: 'Lecture', date: '3/21/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Due: HW11', type: 'Due Date', date: '3/21/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Due: HW12', type: 'Due Date', date: '3/23/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Final Project Ideas/Plan Due', type: 'Final Project', date: '3/24/2023', isDivider: false, isDescription: false},
 
-  {position: 1, name: 'Due: Homework 3', type:'Homework', date: '2/15/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Release Homework 5 (Borrowing, Ownership & Functions)', type:'Homework', date: '2/15/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Due: Homework 4', type:'Homework', date: '2/17/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Release Homework 6 (More ownership practice, with a struct, no impl)', type:'Homework', date: '2/17/2022', isDivider: false, isDescription:false},
+  {position: 1, name: 'Week 10', type:'', date: '', isDivider: true, isDescription:false},
+  {position: 1, name: 'Due: MP3', type: 'Due Date', date: '3/27/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Special Topics Lecture (Traits, Async, Solana, etc...)', type: 'Lecture', date: '3/28/2023', isDivider: false, isDescription:false},
 
-  {position: 1, name:'Week 6', type:'', date: '', isDivider: true, isDescription:false},
-  {position: 1, name: 'Structs Continued - Method Syntax, Generics', type:'Lecture', date: '2/22/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Struct Examples', type:'Lecture', date: '2/24/2022', isDivider: false, isDescription:false},
+  {position: 1, name: 'Week 11', type:'', date: '', isDivider: true, isDescription:false},
+  {position: 1, name: 'Special Topics Lecture (Traits, Async, Solana, etc...)', type: 'Lecture', date: '4/4/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Final Project Checkpoint 1 Due', type: 'Final Project', date: '4/7/2023', isDivider: false, isDescription: false},
 
-  {position: 1, name: 'Release MP2', type:'MP', date: '2/22/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Due: MP1', type:'MP', date: '2/23/2022', isDivider: false, isDescription:false},
+  {position: 1, name: 'Week 12', type:'', date: '', isDivider: true, isDescription:false},
+  {position: 1, name: 'Special Topics Lecture (Traits, Async, Solana, etc...)', type: 'Lecture', date: '4/11/2023', isDivider: false, isDescription:false},
 
-  {position: 1, name: 'Due: Homework 5', type:'Homework', date: '2/22/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Release Homework 7 (basic structs, w/ impl)', type:'Homework', date: '2/22/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Due: Homework 6', type:'Homework', date: '2/24/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Release Homework 8 (more structs w/ impl, maybe generics)', type:'Homework', date: '2/24/2022', isDivider: false, isDescription:false},
+  {position: 1, name: 'Week 13', type:'', date: '', isDivider: true, isDescription:false},
+  {position: 1, name: 'Special Topics Lecture (Traits, Async, Solana, etc...)', type: 'Lecture', date: '4/18/2023', isDivider: false, isDescription:false},
+  {position: 1, name: 'Final Project Checkpoint 2 Due', type: 'Final Project', date: '4/21/2023', isDivider: false, isDescription: false},
 
+  {position: 1, name: 'Week 14', type:'', date: '', isDivider: true, isDescription:false},
+  {position: 1, name: 'Special Topics Lecture (Traits, Async, Solana, etc...)', type: 'Lecture', date: '4/25/2023', isDivider: false, isDescription:false},
 
-  {position: 1, name:'Week 7', type:'', date: '', isDivider: true, isDescription:false},
-  {position: 1, name: 'Parallelism/Concurrency ', type:'Lecture', date: '3/1/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Thread Ownership', type:'Lecture', date: '3/3/2022', isDivider: false, isDescription:false},
-
-  {position: 1, name: 'Release MP3', type:'MP', date: '3/3/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Due: MP2', type:'MP', date: '3/4/2022', isDivider: false, isDescription:false},
-
-  {position: 1, name: 'Due: Homework 7', type:'Homework', date: '3/1/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Release Homework 9 (spawning a single thread to do a task)', type:'Homework', date: '3/1/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Due: Homework 8', type:'Homework', date: '3/3/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Release Homework 10 (thread messaging and thread move)', type:'Homework', date: '3/3/2022', isDivider: false, isDescription:false},
-
-  {position: 1, name:'Week 8', type:'', date: '', isDivider: true, isDescription:false},
-  {position: 1, name: 'Threads & Messaging', type:'Lecture', date: '3/8/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Shared State Messaging', type:'Lecture', date: '3/10/2022', isDivider: false, isDescription:false},
-
-  {position: 1, name: 'Due: Homework 9', type:'Homework', date: '3/8/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Release Homework 11 (spawning a single thread to do a task)', type:'Homework', date: '3/8/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Due: Homework 10', type:'Homework', date: '3/10/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Release Homework 12 (thread messaging and thread move)', type:'Homework', date: '3/10/2022', isDivider: false, isDescription:false},
-
-  {position: 1, name:'Week 8.5', type:'', date: '', isDivider: true, isDescription:false},
-  {position: 1, name:'Spring break!', type:'', date: '', isDivider: false, isDescription:true},
-
-  {position: 1, name:'Week 9', type:'', date: '', isDivider: true, isDescription:false},
-  {position: 1, name: 'Intro to ML', type:'Lecture', date: '3/22/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Final Project Kickoff', type:'Final Project', date: '3/24/2022', isDivider: false, isDescription:false},
-
-  {position: 1, name: 'Due: Homework 11', type:'Homework', date: '3/22/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Due: Homework 12', type:'Homework', date: '3/24/2022', isDivider: false, isDescription:false},
-  
-  {position: 1, name: 'Due: MP3', type:'MP', date: '3/25/2022', isDivider: false, isDescription:false},
-
-  {position: 1, name:'Week 10', type:'', date: '', isDivider: true, isDescription:false},
-  {position: 1, name:'Final Projects', type:'', date: '', isDivider: false, isDescription:true},
-
-  {position: 1, name: 'Debugging Special Lecture', type:'Lecture', date: '3/29/2022', isDivider: false, isDescription:false},
-  {position: 1, name: 'Special Topics Lecture', type:'Lecture', date: '4/1/2022', isDivider: false, isDescription:false},
-
-  {position: 1, name:'Week 11, 12, 13, 14', type:'', date: '', isDivider: true, isDescription:false},
-  {position: 1, name:'Final Projects; No HW, no MPs', type:'', date: '', isDivider: false, isDescription:true},
-  {position: 1, name:'1 Lecture Per Topic per Week. Current Topics: Parallelism, OOP, Functional Programming', type:'', date: '', isDivider: false, isDescription:true},
-
-
-  {position: 1, name:'Week 15', type:'', date: '', isDivider: true, isDescription:false},
-  {position: 1, name: 'Final Project Due', type:'Final Project', date: '5/4/2022', isDivider: false, isDescription:false}
-
+  {position: 1, name: 'Week 15', type:'', date: '', isDivider: true, isDescription:false},
+  {position: 1, name: 'Final Project Due (Reading Day)', type: 'Final Project', date: '5/4/2023', isDivider: false, isDescription: false}
 ];
 
 

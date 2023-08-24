1303cc8f0a5ffc80deba9bd484624e997ef55160
Cooper <kcoopermiller9@gmail.com>
2023-08-23 19:55:53 -0500

    fa23 lectures

--- frontend/src/app/pages/lectures/lectures.component.ts
+++ frontend/src/app/pages/lectures/lectures.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, NgZone, Input } from "@angular/core";
 import { DomSanitizer } from "@angular/platform-browser";
 import { LectureService } from "src/app/services/lecture.service";
+import * as data from "../../../assets/lectures.json";
+import { SafeResourceUrl } from "@angular/platform-browser";
 
 @Component({
   selector: "app-lectures",
@@ -9,34 +11,50 @@ import { LectureService } from "src/app/services/lecture.service";
 })
 export class LecturesComponent implements OnInit {
   @Input() lectureData: string = null;
-  lectures = null;
+  lectures: {
+    LectureID: string;
+    Title: string;
+    Date: string;
+    Slide: string;
+    Link: string | SafeResourceUrl;
+    Comment: string;
+  }[] = data["lectures"];
   currLecture;
   constructor(private sanitizer: DomSanitizer, private NgZone: NgZone, private LectureService: LectureService) { }
 
   ngOnInit() {
-
-    if (this.lectureData) {
-      this.lectures = JSON.parse(this.lectureData)
+    this.LectureService.getLectures().subscribe((lectures) => {
+      this.lectures = this.lectures;
       for (let i = 0; i < this.lectures.length; i++) {
         this.lectures[i].Link = this.sanitizer.bypassSecurityTrustResourceUrl(
-          this.lectures[i].Link
+          this.lectures[i].Link as string
         )
       }
-      // this.populate(data.lectures[data.lectures.length-1]);
-      this.changeLecture(this.lectures.length - 1);
-    } else {
-      this.LectureService.getLectures().subscribe((data) => {
-        console.log(data)
-        this.lectures = data['Lectures']
-        for (let i = 0; i < this.lectures.length; i++) {
-          this.lectures[i].Link = this.sanitizer.bypassSecurityTrustResourceUrl(
-            this.lectures[i].Link
-          )
-        }
-        // this.populate(data.lectures[data.lectures.length-1]);
-        this.changeLecture(this.lectures.length - 1);
-      })
-    }
+      // console.log("HERE!" + this.lectures[0].Title);
+    })
+
+    // if (this.lectureData) {
+    //   this.lectures = JSON.parse(this.lectureData)
+    //   for (let i = 0; i < this.lectures.length; i++) {
+    //     this.lectures[i].Link = this.sanitizer.bypassSecurityTrustResourceUrl(
+    //       this.lectures[i].Link
+    //     )
+    //   }
+    //   // this.populate(data.lectures[data.lectures.length-1]);
+    //   this.changeLecture(this.lectures.length - 1);
+    // } else {
+    //   this.LectureService.getLectures().subscribe((data) => {
+    //     console.log(data)
+    //     this.lectures = data['Lectures']
+    //     for (let i = 0; i < this.lectures.length; i++) {
+    //       this.lectures[i].Link = this.sanitizer.bypassSecurityTrustResourceUrl(
+    //         this.lectures[i].Link
+    //       )
+    //     }
+    //     // this.populate(data.lectures[data.lectures.length-1]);
+    //     this.changeLecture(this.lectures.length - 1);
+    //   })
+    // }
 
   }
 
